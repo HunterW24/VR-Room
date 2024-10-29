c340292ad583096943e5e9b7b5ab239de6c671ad
HunterW <146263396+HunterW24@users.noreply.github.com>
2024-10-29 15:02:58 -0500

    shapes

--- Assets/Challenges/02_3DPainting/Scripts/PedestalTurn.cs
+++ Assets/Challenges/02_3DPainting/Scripts/PedestalTurn.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PedestalTurn : MonoBehaviour
+public class RotateShapePrefabs : MonoBehaviour
 {
-    public Slider rotationSlider;
+    public Slider rotationSlider; // Reference to the UI Slider
     public GameObject pedestal;
 
-    // Start is called before the first frame update
     void Start()
     {
-   
+        // Assuming the Slider's initial value is 0
+        rotationSlider.onValueChanged.AddListener(RotateShapes);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
         float rotationAngle = rotationSlider.value * 180f; // Adjust the multiplier if needed
         pedestal.transform.rotation = Quaternion.Euler(0f, rotationAngle, 0f);
+    }
+
+    void RotateShapes(float value)
+    {
+        // Find all GameObjects with the tag "Shape"
+        GameObject[] shapeObjects = GameObject.FindGameObjectsWithTag("Shape");
+
+        // Iterate through each shape object
+        foreach (GameObject shapeObject in shapeObjects)
+        {
+            // Get the Rigidbody component
+            Rigidbody rb = shapeObject.GetComponent<Rigidbody>();
+
+            // Rotate the Rigidbody around the Y-axis
+            rb.angularVelocity = new Vector3(0f, rotationSlider.value , 0f); // Adjust the multiplier as needed
+        }
     }
 }
